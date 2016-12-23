0b7340da0a307225497d46a2a1c5045972fd4f1a
rjc131 <rjc131@live.com>
2016-12-22 16:45:57 -0800

    Password char.

--- FormMoveCS/Login.Designer.cs
+++ FormMoveCS/Login.Designer.cs
@@ -212,7 +212,7 @@
             this.textBox2.Location = new System.Drawing.Point(142, 223);
             this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox2.Name = "textBox2";
-            this.textBox2.PasswordChar = '&';
+            this.textBox2.PasswordChar = '*';
             this.textBox2.Size = new System.Drawing.Size(274, 26);
             this.textBox2.TabIndex = 6;
             // 
